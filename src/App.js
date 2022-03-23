900effdecf528207d0ed41fa853e7fc8452ffc64
Rahmat Alfianto <rrahmatalfianto@gmail.com>
2022-03-23 16:53:01 +0700

    Adding a test file to branch_cobacoba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import data from "./data/dataFromSpotify.js";
 
 function App() {
+  const {
+    album: {
+      images: [{ url: albumImgUrl }],
+      name: albumName,
+    },
+    artists: [{ name: artistName }],
+    name: songTitle,
+  } = data;
+  console.log(data);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sopify Coba-Coba
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className="main">
+        <section className="album-container">
+          <img src={albumImgUrl} alt={albumName} className="album-image" />
+          <div className="song-info">
+            <h1 className="song-title">{songTitle}</h1>
+            <h2 className="song-artist">{artistName}</h2>
+            <button type="button" className="btn btn--select">
+              Select
+            </button>
+          </div>
+        </section>
+      </main>
+      <footer className="footer">
+        <p>
+          Generasi Gigih FE - 2022 - Rahmat Alfianto
+        </p>
+      </footer>
     </div>
   );
 }
